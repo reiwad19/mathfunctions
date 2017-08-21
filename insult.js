cb928146151366aed492d601d8ada19fd228d951
reiwad19 <30877953+reiwad19@users.noreply.github.com>
2017-08-21 10:48:50 -1000

    insult generator

---
+++ insult.js
@@ -0,0 +1,14 @@
+
+function insult() {
+
+var userNAme = prompt("Enter your name");
+
+var insultOne = [
+  "My grandma can run faster than you",
+  "who do you think you are? chris froome wannabe",
+  ];
+(Math.floor(Math.random() * 2));
+var x = (Math.floor(Math.random() * 2));
+window.alert(userName + insultone[x]);
+
+}
